22e8c9bef46f73a37eb2a5ee988f535b57277438
namanj2003 <namankjain2003@gmail.com>
2024-01-09 22:52:58 +0530

    UI screenshots added.
    Added screenshots of the UI for better understanding of how the project looks.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer style={{ textAlign: "center", bottom: "0", width: "100%", height: "60px", backgroundColor: "#FFD8B1", paddingTop: "20px" }}>
-      <p>&copy; 2023 Tech News</p>
+      <p>&copy; 2024 Tech News</p>
     </footer>
   );
 }
